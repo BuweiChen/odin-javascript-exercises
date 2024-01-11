4675b916b3366b78e2e81a72e5a8dbfc826a2604
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-11 17:51:48 -0600

    Solve exercises 1-4, 4 was especially difficult
    Learned how to handle optional arguments using "arguments" object.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let toRemoveArr = Array.prototype.slice.call(arguments, 1);
+    for (let i = 0; i < toRemoveArr.length; i++) {
+        if (arr.includes(toRemoveArr[i])) {
+            arr.splice(arr.indexOf(toRemoveArr[i]), 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
