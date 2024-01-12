9f128821e1990be4728e03d0830ea1ec4a6af5b6
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-11 23:23:43 -0600

    Finish rest of exercises
    The output formatting of tempConversion feels janky. There has to
    be better ways to show only one decimal point in javascript. I can
    think of 1 million ways in C++;

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    let smaller = num1 > num2 ? num2 : num1;
+    let bigger = num1 > num2 ? num1 : num2;
+    let sum = 0;
+    for (; smaller <= bigger; smaller++) {
+        sum += smaller;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInF) {
+  return Math.round((degreesInF - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInC) {
+  return Math.round((degreesInC / 5 * 9 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
