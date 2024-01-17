e81d420a6afd7366e1949ce0e17dccf41fe8b529
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-01-17 14:11:08 -0500

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const letters_and_numbers = [...str.toLowerCase()].filter(char => /^[a-z0-9]$/.test(char));
+    return letters_and_numbers.join() == letters_and_numbers.reverse().join();
 };
 
 // Do not edit below this line
